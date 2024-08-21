67b5335aee6109b082050e9390cc46f94c654356
Kwuang Tang <arctangent20@yahoo.com>
2024-08-21 15:51:07 -0400

    add some css

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col h-screen border-green-500 p-0 m-0">
+        <div className="flex justify-between items-center w-full p-4 bg-indigo-100 border-b-2 border-indigo-100">
+          <span className="text-xl font-bold">Trello Clone</span>
+          <div className="flex gap-4">
+            <button className="text-black border-2 border-blue-500 hover:bg-blue-700 text-sm font-bold py-2 px-4 rounded">
+              Home
+            </button>
+            <button className="text-black border-2 border-green-500 hover:bg-green-700 text-sm font-bold py-2 px-4 rounded">
+              About
+            </button>
+            <button className="text-black border-2 border-red-500 hover:bg-red-700 text-sm font-bold py-2 px-4 rounded">
+              Contact
+            </button>
+          </div>
+        </div>
+        <div className="bg-indigo-100 border-2 border-indigo-200 flex items-center justify-center w-full h-full">
+          <Board />
+        </div>
       </div>
-      <h1>asdf</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+  );
+}
+
+function Board() {
+  return (
+    <div className="bg-gray-200 border-2 border-indigo-200 flex items-start justify-center w-full h-full flex-row gap-4 m-4 p-4">
+      <CardColumn />
+      <CardColumn />
+      <CardColumn />
+    </div>
+  );
+}
+
+function CardColumn() {
+  return (
+    <div className="bg-indigo-100 border-2 border-indigo-200 flex flex-col items-start justify-center w-80 h-full gap-4 p-4 rounded-xl">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+function Card() {
+  return (
+    <div className="bg-white border-2 border-indigo-200 flex items-start justify-start rounded-xl w-full py-6">
+      <span className="text-black text-md">foobar</span>
+    </div>
   );
 }
 
